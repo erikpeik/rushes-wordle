2abdd27fc59a0fbf07e04a33bca403a75b263fee
Alba Castano <acastano@student.hive.fi>
2022-02-23 20:23:38 +0200

    wordle.c changes: checks guess against word, stores chars info(in the making)

--- wordle.c
+++ wordle.c
@@ -6,7 +6,7 @@
 /*   By: acastano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:24:23 by acastano          #+#    #+#             */
-/*   Updated: 2022/02/23 15:17:31 by acastano         ###   ########.fr       */
+/*   Updated: 2022/02/23 18:27:55 by acastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,52 +23,78 @@ List of answers
 Starting word - feedback
 remove words with letters marked out, and letters not in correct possition
 
-
-int	ft_is_letter(char *word, char *guess)//alter, crawl
+int	ft_is_letter(char *word, char *guess)
 {
 	int	i;
+	int	j;
+
 	i = 0;
+	j = 0;
 	while (word[i])
 	{
 		while (guess[j])
 		{
 			if (ft_strchr(word, guess[j]))
 			{
-				ft_save_letter(guess[j]);
+				return (1);
 			}
 			j++;
 		}
 		i++;
 		}
-	return ();
-	}
-
-//int	ft_display_list(char *guess);
-*/
-
-int	ft_check_letters(char *word, char *answer)//fix
-{
-	char	*yes;
-	char	*maybe;
-	char	*not;
-	if (ft_is_letter(word, answer) == 1)
-		return (1);//same??
 	return (0);
 	}
+*/
+
+int	ft_is_letteri(char *word, char *c)//returns index where it finds letter, or 5 if it doesn't find it
+{
+	int	i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (word[i] == c)
+			break;
+		i++;
+	}
+	return (i);
+}
 
 int	ft_get_info(char *word, char *answer)//fix
 {
-	static char	*info[4];
+	char	*info[4];
+//	char	*letters;//[27] = "abcdefghijklmnopqrstuvwxyz";//26
 	char	*yes;
-	char	*maybe;
+	char	*somewhere;
 	char	*not;
 
+//	letters = "abcdefghijklmnopqrstuvwxyz";//26 + \0
+	yes = ft_strnew(5);
+	somewhere = ft_strnew(5);
+	not = '\0';
+//	info[0] = letters;
 	info[0] = yes;
-	info[1] = maybe;
+	info[1] = somewhere;
 	info[2] = not;
-	info[3] = '\0';
-	if (ft_is_letter(word, answer, info) == 1)
-		return (1);//same??
+	info[3] = NULL;
+	int	index = 0;
+	int	curr = 0;
+	while (i < 5)
+	{
+		curr = ft_is_letteri(word, answer[i]);
+		if (curr == i)
+			yes[i] = answer[i];
+		else if (curr < 5)
+		{
+			somewhere[i] = answer[i];
+		}
+		else
+		{
+			not = ft_strjoin(not, answer[i]);
+//take it from letters?
+		}
+		i++;
+	}
 	return (0);
 }
 
