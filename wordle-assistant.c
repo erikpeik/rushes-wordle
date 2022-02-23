e1e5fad5f1a12e5163f579b4e638ee39177e1f52
Erik Mende <emende@student.hive.fi>
2022-02-23 18:48:05 +0200

    modified wordle-assistant.c

--- wordle-assistant.c
+++ wordle-assistant.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 13:25:55 by emende            #+#    #+#             */
-/*   Updated: 2022/02/23 17:36:11 by emende           ###   ########.fr       */
+/*   Updated: 2022/02/23 18:31:08 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_lstpop(t_list **alst, char *content)
 	}
 	while (temp != NULL)
 	{
-		if (ft_strcmp((const char *) (temp->next)->content, (char const *) content) == 0)
+		if (temp->next && ft_strcmp((const char *) (temp->next)->content, (char const *) content) == 0)
 		{
 			next = temp->next->next;
 			ft_memdel((void **) &(temp->next)->content);
@@ -51,22 +51,39 @@ void	ft_lstadd_back(t_list **begin_list, t_list *new)
 	list->next = new;
 }
 
+void print_list(t_list *list)
+{
+	t_list	*temp;
+
+	temp = list;
+	while (temp)
+	{
+		ft_putendl((char const *) temp->content);
+		temp = temp->next;
+	}
+}
+
+void remove_blank(t_list **list, char *guess)
+{
+
+}
+
 int	main(void)
 {
 	int		fd;
 	int		i;
-/*	int		round; */
+	int		round;
 	char	*line;
-/*	char	guess[10];
+	char	guess[10];
 	char	feedback[10];
-	char	*array[ALLOWED_COUNT]; */
 	t_list	*word_list;
 	t_list *temp;
 
-	fd = open("test.txt", O_RDONLY);
+	fd = open("easy_test.txt", O_RDONLY);
 	if (fd < 0)
 	{
 		ft_putstr_fd("error\n", 2);
+		return (-1);
 	}
 	i = 0;
 	get_next_line(fd, &line);
@@ -83,24 +100,18 @@ int	main(void)
 	temp = word_list;
 /*	temp = temp->next->next; */
 	ft_lstpop(&word_list, "five");
-	while (word_list != NULL)
-	{
-		ft_putendl((char const *) word_list->content);
-		word_list = word_list->next;
-	}
-/*	while (array[i])
-	{
-		ft_putendl((const char *) array[i++]);
-	} */
-/*	round = 1;
+//	print_list(word_list);
+	round = 1;
 	while (round <= 5)
 	{
 		printf("Guess %d: ", round);
 		scanf("%s", guess);
-		printf("Your guess: %s\n", guess);
-		printf("Feedback %d: ", round);
+//		remove_blank(&word_list, guess);
+//		printf("Your guess: %s\n", guess);
+	printf("Feedback %d: ", round);
 		scanf("%s", feedback);
-		printf("Your feedback: %s\n", feedback);
+//		printf("Your feedback: %s\n", feedback);
+		print_list(word_list);
 		round++;
-	} */
+	}
 }
